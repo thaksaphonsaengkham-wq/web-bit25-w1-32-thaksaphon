a9e169064d176b7e2a85f7341765616199bc57c5
USER922-32 <user@USER922-32.local>
2026-06-05 10:55:17 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+</head>
+<body>
+      
+      <h1>งานที่1 ทักษพร แสงคำ BIT. 2/5 เลขที่32</h1>
+
+      <form action="">
+            <label for="">เลขแม่สูตรคูณ</label> <br>
+            <input type="number" name="num" id="">
+
+            <input type="submit" value="คำนวณ">
+      </form>
+
+      <?php
+            if(isset($_GET["num"])){
+                  $num = $_GET["num"];
+
+                  echo "สูตรคูณแม่ " . $num;
+            }
+      ?>
+
+</body>
+</html>
