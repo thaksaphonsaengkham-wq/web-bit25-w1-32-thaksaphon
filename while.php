687cccef0814e820377b65b94bd3819e1cd25624
USER922-32 <user@USER922-32.local>
2026-06-12 11:02:20 +0700

    while loop

--- while.php
+++ while.php
@@ -3,31 +3,30 @@
 <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>แม่สูตรคูณ (For Loop)</title>
+      <title>แม่สูตรคูณ (While Loop)t</title>
       <link rel="stylesheet" href="style.css">
 </head>
 <body>
       
       <h1>งานที่1 ทักษพร แสงคำ BIT. 2/5 เลขที่32</h1>
-      <a href="while.php">While Loop</a>
+      <a href="index.php">For Loop</a>
 
       <form action="">
             <label for="">เลขแม่สูตรคูณ</label> <br>
             <input type="number" name="num" id="">
-
             <input type="submit" value="คำนวณ">
       </form>
 
       <?php
             if(isset($_GET["num"])){
-                  $num = $_GET["num"];
+                $num = $_GET["num"];
 
-                  echo "สูตรคูณแม่ " . $num ."<br>";
-
-                  //.  เริ่ม          จบ.    เพิ่มทีละ1
-                  for($i = 1;  $i <= 12; $i++){
-                        echo $num . "x" . $i . "=" . $num * $i . "<br>";
-                  }
+                echo "สูตรคูณแม่ " . $num ."<br>";
+                $i = 1;
+                while ($i <= 12){
+                    echo $num . "x" . $i . "=" . $num * $i . "<br>";
+                    $i++;
+                }
             }
       ?>
 
